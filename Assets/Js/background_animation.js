44c18f1cb9c05586c87e182edf5d0373a520de7e
Roodri02 <rodrigocabellohernandez07@gmail.com>
2025-01-19 19:31:23 +0100

    Ajuste de imagenes

--- Assets/Js/background_animation.js
+++ Assets/Js/background_animation.js
@@ -1,18 +1,35 @@
 const slides = document.querySelector('.slides');
+const slides2 = document.querySelector('.slides2');
 const totalSlides = document.querySelectorAll('.slide').length;
-let currentIndex = 0;
+const totalSlides2 = document.querySelectorAll('.slide2').length;
+let currentIndex1 = 0;
+let currentIndex2 = 0;
 
 function slideImages() {
-    currentIndex++;
+    currentIndex1++;
+    currentIndex2++;
     
-    if (currentIndex === totalSlides) {
+    if (currentIndex1 === totalSlides) {
         slides.style.transition = 'none';
         slides.style.transform = 'translateX(0)';
-        currentIndex = 0;
+        currentIndex1 = 0;
     } else {
         
         slides.style.transition = 'transform 1s ease-in-out';
-        slides.style.transform = `translateX(-${(100/totalSlides)*currentIndex}%)`;
+        slides.style.transform = `translateX(-${(100/totalSlides)*currentIndex1}%)`;
+    }
+    console.log(totalSlides2);
+    if (currentIndex2 === totalSlides2) {
+
+
+        slides2.style.transition = 'none';
+        slides2.style.transform = 'translateX(0)';
+        currentIndex2 = 0;
+    } 
+    else {
+        
+        slides2.style.transition = 'transform 1s ease-in-out';
+        slides2.style.transform = `translateX(-${(100/totalSlides2)*currentIndex2}%)`;
     }
 }
 
