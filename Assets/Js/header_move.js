e33a0dc75ad6f0ce3b4ac52623fbddb348065a50
Roodri02 <rodrigocabellohernandez07@gmail.com>
2025-01-19 22:53:23 +0100

    Scroll header animation works

--- Assets/Js/header_move.js
+++ Assets/Js/header_move.js
@@ -3,6 +3,7 @@ const header = document.querySelector('.header');
 
 // Agrega un evento de scroll
 window.addEventListener('scroll', () => {
+  console.log("Hola");
   if (window.scrollY > 50) {
     header.style.backgroundColor = '#ffffff';
     header.style.color = '#000000';
