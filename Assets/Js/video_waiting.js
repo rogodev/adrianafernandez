cf7fec45c441de43ce361c12d4d323c65e922ebc
Roodri02 <rodrigocabellohernandez07@gmail.com>
2025-02-09 21:54:29 +0100

    Ahora si

--- Assets/Js/video_waiting.js
+++ Assets/Js/video_waiting.js
@@ -10,18 +10,12 @@
                 // Si todos los videos están listos, ocultamos la pantalla de carga
                 loadingScreen.style.display = 'none';
 
-                // Cuando los videos estén cargados, lanzamos el evento load para la transición
-                window.dispatchEvent(new Event('load'));
+                const transitionScreen = document.querySelector('.transition-screen-out');
+                transitionScreen.style.top = '100%';
             }
         }
 
         // Agregamos un listener para cada video para comprobar cuando estén listos
         videos.forEach((video) => {
             video.addEventListener('canplaythrough', checkVideosLoaded, { once: true });
-        });
-
-        // Aquí va el código de la animación de transición cuando los videos estén listos
-        window.addEventListener('load', () => {
-            const transitionScreen = document.querySelector('.transition-screen-out');
-            transitionScreen.style.top = '100%';
         });
